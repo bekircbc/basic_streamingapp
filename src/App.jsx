d3e90e1c1ca45495f34c26cc4173a53bc23f5466
Bekir - CDI <b.burakcebecii@gmail.com>
2022-04-20 11:06:00 +0200

    npm i sass

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./App.scss";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">Video Film streaming App</div>;
+  return (
+    <div className="App">
+      <h1>Video Film streaming App</h1>
+    </div>
+  );
 }
 
 export default App;
