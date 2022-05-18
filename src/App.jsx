d9bf63e47fe1068c6026d196a4090eb3661bcfd7
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-19 00:39:05 +0200

    main navigation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.scss";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import { PageAllMeetups } from "./components/PageAllMeetups";
 import { PageFavorites } from "./components/PageFavorites";
 import { PageNewMeetup } from "./components/PageNewMeetup";
+import { MainNavigation } from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div className="App">
       <h1>Video Film Streaming App</h1>
+      <MainNavigation />
       <Switch>
         <Route path="/" element={<PageAllMeetups />} />
         <Route path="/newmeetup" element={<PageNewMeetup />} />
