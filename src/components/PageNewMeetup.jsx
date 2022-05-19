c235e1cce987e99d725283f772eca27d59fc01e6
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-19 10:34:56 +0200

    changed name of decscription label..

--- src/components/PageNewMeetup.jsx
+++ src/components/PageNewMeetup.jsx
@@ -12,7 +12,7 @@ export function PageNewMeetup() {
         <label htmlFor="address">Address</label>
         <input type="text" required id="address" />
 
-        <label htmlFor="description">Meetup Title</label>
+        <label htmlFor="description">Meetup Description</label>
         <textarea id="description" required rows="5"></textarea>
 
         <button>Add Meetup</button>
