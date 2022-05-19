0ed6fd2778cabf1a433b7e27825c26fdcc5c3dda
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-19 08:55:19 +0200

    Dummy data to pageallmetup added

--- src/components/PageAllMeetups.jsx
+++ src/components/PageAllMeetups.jsx
@@ -1,3 +1,22 @@
+const DummyData = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image: "../../public/images/meetup.jpg",
+    adresss: "Meetupstreet5, 12345 Meetup City",
+    description:
+      "This is a first amazing meetup which you definitely should not miss.",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image: "../../public/images/meetup.jpg",
+    adresss: "Meetupstreet5, 12345 Meetup City",
+    description:
+      "This is a second amazing meetup which you definitely should not miss.",
+  },
+];
+
 export function PageAllMeetups() {
   return <div>All Meetups Page</div>;
 }
