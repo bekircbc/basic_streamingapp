c43361cef13f34f9b8839ef1ab1ef393a1bcb680
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-19 11:52:01 +0200

    getting loadedMeetup data from firebase

--- src/components/PageAllMeetups.jsx
+++ src/components/PageAllMeetups.jsx
@@ -1,45 +1,63 @@
-const DummyData = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image: "../../public/images/meetup.jpg",
-    adress: "Meetupstreet5, 12345 Meetup City",
-    description:
-      "This is a first amazing meetup which you definitely should not miss.",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image: "../../public/images/meetup.jpg",
-    adress: "Meetupstreet5, 12345 Meetup City",
-    description:
-      "This is a second amazing meetup which you definitely should not miss.",
-  },
-];
+import { useState } from "react";
+// const DummyData = [
+//   {
+//     id: "m1",
+//     title: "This is a first meetup",
+//     image: "../../public/images/meetup.jpg",
+//     adress: "Meetupstreet5, 12345 Meetup City",
+//     description:
+//       "This is a first amazing meetup which you definitely should not miss.",
+//   },
+//   {
+//     id: "m2",
+//     title: "This is a second meetup",
+//     image: "../../public/images/meetup.jpg",
+//     adress: "Meetupstreet5, 12345 Meetup City",
+//     description:
+//       "This is a second amazing meetup which you definitely should not miss.",
+//   },
+// ];
 
-export function PageAllMeetups() {
+export async function PageAllMeetups() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+  await fetch(
+    "https://basic-streaming-app-default-rtdb.firebaseio.com/meetups.json"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setIsLoading(false);
+      setLoadedMeetups(data);
+    });
+
   return (
     <section className="allMeetupsPage">
       <h1>All Meetups</h1>
-      <ul className="allMeetups">
-        {DummyData.map((meetup) => {
-          return (
-            <li key={meetup.id}>
-              <div>
-                <img src={meetup.image} alt="" />
-              </div>
-              <div>
-                <h3>{meetup.title}</h3>
-                <address>{meetup.adress}</address>
-                <p>{meetup.description}</p>
-              </div>
-              <div>
-                <button>To Favorites</button>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      {{ isLoading } ? (
+        <p>Loading...</p>
+      ) : (
+        <ul className="allMeetups">
+          {loadedMeetups.map((meetup) => {
+            return (
+              <li key={meetup.id}>
+                <div>
+                  <img src={meetup.image} alt="" />
+                </div>
+                <div>
+                  <h3>{meetup.title}</h3>
+                  <address>{meetup.adress}</address>
+                  <p>{meetup.description}</p>
+                </div>
+                <div>
+                  <button>To Favorites</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </section>
   );
 }
