a561ccc7cdaaaa85b97d14c88b5c1748ffa2bce8
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-19 10:49:16 +0200

    added submitHandler, onSubmit and useRef

--- src/components/PageNewMeetup.jsx
+++ src/components/PageNewMeetup.jsx
@@ -1,19 +1,44 @@
+import { useRef } from "react";
+
 export function PageNewMeetup() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+  }
   return (
     <section className="newMeetupPage">
       <h1>New Meetup Page</h1>
-      <form className="formCard">
+      <form className="formCard" onSubmit={submitHandler}>
         <label htmlFor="title">Meetup Title</label>
-        <input type="text" required id="title" />
+        <input type="text" required id="title" ref={titleInputRef} />
 
         <label htmlFor="image">Meetup Image</label>
-        <input type="url" required id="image" />
+        <input type="url" required id="image" ref={imageInputRef} />
 
         <label htmlFor="address">Address</label>
-        <input type="text" required id="address" />
+        <input type="text" required id="address" ref={addressInputRef} />
 
         <label htmlFor="description">Meetup Description</label>
-        <textarea id="description" required rows="5"></textarea>
+        <textarea
+          id="description"
+          required
+          rows="5"
+          ref={descriptionInputRef}
+        ></textarea>
 
         <button>Add Meetup</button>
       </form>
