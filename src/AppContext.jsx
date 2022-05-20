c3f333c2b532e50d83cc5ca355b178b1a80f33ec
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-20 13:33:54 +0200

    context to PageFavorites added

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -59,6 +59,7 @@ export const AppProvider = ({ children }) => {
       value={{
         isLoading,
         loadedMeetups,
+        context,
       }}
     >
       {children}
--- src/components/PageFavorites.jsx
+++ src/components/PageFavorites.jsx
@@ -1,3 +1,7 @@
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+
 export function PageFavorites() {
+  const { context } = useContext(AppContext);
   return <div>Favorites Page</div>;
 }
